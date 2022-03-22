9d81abb7bf064f85ac1e96c59853fbf8b4271dd1
AndreaGuarracino <andreaguarracino@outlook.com>
2022-03-22 20:56:11 +0100

    fix

--- scripts/msmc2.sh
+++ scripts/msmc2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PATH_POPULATION_TXT=$1
-PATH_HACKED_GENERATE_MULTIHETSEP_PY=$3
+PATH_HACKED_GENERATE_MULTIHETSEP_PY=$2
 PATH_MSMC2=$3
 PREFIX=$4
 
@@ -11,13 +11,7 @@ nsamples_minus_1=$(echo "$nsamples - 1" | bc)
 samples=$(cat $PATH_POPULATION_TXT | tr '\n' ' ' | sed 's/.$//')
 
 # Generate files for MSMC
-# Using a hacked version, changing a few rows in generate_multihetsep.py to get phased fake-diploid data
-#print ("HACKED: Non-diploid SNP found and considered as phased data: %s" % geno, file=sys.stderr)
-#phased = True
-#geno = "%s|%s" % (geno[0], geno[0])
-
-# '#' breaks MSMC
-python3 $PATH_HACKED_GENERATE_MULTIHETSEP_PY $samples | sed 's/#/-/g' > $PREFIX.multihetsep
+python3 $PATH_HACKED_GENERATE_MULTIHETSEP_PY $samples > $PREFIX.multihetsep
 
 # Estimating the effective population size
 haplos=`seq 0 $nsamples_minus_1 | tr '\n' ',' | sed 's/.$//'`
