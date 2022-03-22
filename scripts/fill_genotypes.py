813362cdda007f91d06867641b178199b9215313
AndreaGuarracino <andreaguarracino@outlook.com>
2022-03-23 00:09:08 +0100

    fill diploid genotypes

--- scripts/fill_genotypes.py
+++ scripts/fill_genotypes.py
@@ -1,5 +1,7 @@
 import sys
 
+diploid = sys.argv[1] == 'diploid'
+
 for line in sys.stdin:
     if line.startswith('#'):
         print(line.strip())
@@ -8,9 +10,14 @@ for line in sys.stdin:
 
         for i, gt in enumerate(line_list[9:]):
             new_gt = ''
+            if diploid and len(gt) != 3:
+                if gt in ['01']:
+                    new_gt == f'{gt}|{gt}'
+                else:
+                    new_gt == '0|0'
             for j, c in enumerate(gt):
                 if j % 2 == 0 and c == '.':
-                        c = '0'
+                    c = '0'
                 new_gt += c
             
             line_list[9 + i] = new_gt
