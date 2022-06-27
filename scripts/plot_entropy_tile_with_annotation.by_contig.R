1dc67effc446013c5220b91449629ce0840b326b
AndreaGuarracino <andreaguarracino@outlook.com>
2022-06-27 18:27:39 +0200

    update entropy by chromosome

--- scripts/plot_entropy_tile_with_annotation.by_contig.R
+++ scripts/plot_entropy_tile_with_annotation.by_contig.R
@@ -13,7 +13,7 @@ library(ggforce)
 
 x <- read.delim(path_entropy_tsv)
 
-x <- x[x$shannon_div_index != -1, ]
+x[x$shannon_div_index == -1, ]$shannon_div_index = NA
 
 if (num_chr == 13) {
   colors <- c("#F8766D")
@@ -29,12 +29,18 @@ if (num_chr == 13) {
   colors <- c("#000000")
 }
 
+library(dplyr)
+# Compute average SDI by window by ground.target
+y <- x %>% 
+  dplyr::group_by(ground.target, start.pos, end.pos) %>% 
+  dplyr::summarize(average_sdi = mean(shannon_div_index, na.rm = TRUE))
 
 # Apply filters
 chr <- paste0('chm13#chr', num_chr)
 xx <- x[x$ground.target == chr,]
+yy <- y[y$ground.target == chr,]
 
-p <- ggplot(xx,
+p <- ggplot(xx[!is.na(xx$shannon_div_index),],
     aes(
         x = start.pos + (end.pos - start.pos) / 2,
         width = end.pos - start.pos - 200,
@@ -99,22 +105,48 @@ ggsave(
 
 
 # Aggregate entropy by chromosome (averaging the SDI across contigs for each window)
-library(dplyr)
-yy <- xx %>% 
-  dplyr::group_by(ground.target, start.pos, end.pos) %>% 
-  dplyr::summarize(average_sdi = mean(shannon_div_index))
+if (FALSE) {
+  p <- ggplot(yy[!is.na(yy$average_sdi),],
+              aes(
+                x = start.pos + (end.pos - start.pos) / 2,
+                width = end.pos - start.pos - 200,
+                y = ground.target, alpha=average_sdi,
+                fill = ground.target
+              )
+  ) +
+    geom_tile() +
+    #ggtitle(title) +
+    facet_col(~ground.target, scales = "free_y", space = "free", labeller = labeller(variable = labels)) +
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      
+      text = element_text(size = 32),
+      axis.text.x = element_text(size = 18),
+      axis.text.y = element_text(size = 18),
+      
+      legend.title = element_text(size = 32),
+      legend.text = element_text(size = 32),
+      legend.position = "top",
+      
+      plot.margin = margin(0, 0.2, 0, 3.07, "cm")
+      
+      #axis.title.y=element_blank()
+    ) +
+    labs(alpha="Average SDI", fill="Target") +
+    scale_x_continuous(limits = c(x_min, x_max), expand = c(0, 0)) +
+    scale_fill_manual(values=colors) +
+    labs(x ="Position", y = '')
+  #p
+}
 
-p <- ggplot(yy,
-            aes(
-              x = start.pos + (end.pos - start.pos) / 2,
-              width = end.pos - start.pos - 200,
-              y = ground.target, alpha=average_sdi,
-              fill = ground.target
-            )
-) +
-  geom_tile() +
-  #ggtitle(title) +
-  facet_col(~ground.target, scales = "free_y", space = "free", labeller = labeller(variable = labels)) +
+
+library(scales) # for pretty_breaks()
+p <- ggplot(yy, aes(x=start.pos + (end.pos - start.pos) / 2, y=average_sdi, color=ground.target)) +
+  geom_step() +
+  scale_x_continuous(limits = c(x_min, x_max), expand = c(0, 0, 0, 0)) +
+  scale_y_continuous(limits = c(0, max(2, max(y$average_sdi))), breaks=pretty_breaks()) +
+  facet_wrap(~ground.target, scales = "free", ncol=1, labeller = labeller(variable = labels)) +
+  theme_bw() +
   theme(
     plot.title = element_text(hjust = 0.5),
     
@@ -122,24 +154,24 @@ p <- ggplot(yy,
     axis.text.x = element_text(size = 18),
     axis.text.y = element_text(size = 18),
     
-    legend.title = element_text(size = 32),
-    legend.text = element_text(size = 32),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 20),
     legend.position = "top",
     
-    plot.margin = margin(0, 0.2, 0, 3.07, "cm")
-    
-    #axis.title.y=element_blank()
+    strip.text.x = element_blank(),
+    strip.text.y = element_blank(),
+    plot.margin = unit(c(0,0.4,0,6.3), "cm")
+  ) + labs(
+    x = paste(chr, 'position'),
+    y = paste('')
   ) +
-  labs(alpha="Average SDI", fill="Target") +
-  scale_x_continuous(limits = c(x_min, x_max), expand = c(0, 0)) +
-  scale_fill_manual(values=colors) +
-  labs(x ="Position", y = '')
+  guides(colour = guide_legend(override.aes = list(size=10)))
 #p
 
 p_with_annotation <- ggpubr::ggarrange(
   ggplotted_img, p,
   labels=c('', ''),
-  heights = c(height_bar*8, height_bar*max(7, length(unique(yy$ground.target))) / 2),
+  heights = c(height_bar*8, height_bar*max(7, length(unique(yy$ground.target))) / 1.7),
   legend = "right", # legend position,
   common.legend = T,
   nrow = 2
@@ -147,7 +179,7 @@ p_with_annotation <- ggpubr::ggarrange(
 ggsave(
   plot = p_with_annotation,
   paste0(prefix_output, '.by_chromosome.pdf'),
-  width = width, height = (5+max(7, length(unique(yy$ground.target)))) * height_bar,
+  width = width, height = (6+max(7, length(unique(yy$ground.target)))) * height_bar,
   units = "cm",
   dpi = 100, bg = "white",
   limitsize = FALSE
