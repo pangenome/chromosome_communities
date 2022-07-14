340239b16cb5ecbd7d08a75f72df5f2f273b2961
AndreaGuarracino <andreaguarracino@outlook.com>
2022-07-14 21:41:56 +0200

    update script to include num community

--- scripts/get_table_about_communities.py
+++ scripts/get_table_about_communities.py
@@ -47,16 +47,16 @@ for num, label_list in num_2_labels_dict.items():
         counter_list[set_chr_nr(label) - 1] = count
 
     counter_list = [str(x) for x in counter_list]
-    if (num, chr_with_max_count) not in community_2_info_dict:
-        community_2_info_dict[(num, chr_with_max_count)] = []
-    community_2_info_dict[(num, chr_with_max_count)].append(counter_list)
+    if chr_with_max_count not in community_2_info_dict:
+        community_2_info_dict[chr_with_max_count] = []
+    community_2_info_dict[chr_with_max_count].append((num, counter_list))
 
     #print('\t'.join([chr_with_max_count] + counter_list))
 
 chromosomes = community_2_info_dict.keys()
-for num, chr in sorted(chromosomes, key=lambda x: set_chr_nr(x[1])):
-    for i, info in enumerate(community_2_info_dict[(num, chr)]):
+for chr in sorted(chromosomes, key=lambda x: set_chr_nr(x)):
+    for i, (num, counter_list) in enumerate(community_2_info_dict[chr]):
         suffix = ''
-        if len(community_2_info_dict[(num, chr)]) > 1:
+        if len(community_2_info_dict[chr]) > 1:
             suffix = f'_{i+1}'
-        print('\t'.join([num] + [chr + suffix] + info))
+        print('\t'.join([chr + suffix] + counter_list))
