64899b70e70d8a1d86b827f6e0ec99987790d8db
AndreaGuarracino <andreaguarracino@outlook.com>
2022-11-13 20:30:56 -0600

    update script

--- scripts/plot_untangle_with_annotation_and_flagger.R
+++ scripts/plot_untangle_with_annotation_and_flagger.R
@@ -35,9 +35,9 @@ if (sum(x$jaccard > 1) > 0) {
 }
 
 # From https://doi.org/10.1093/bioinformatics/btac244
-#x$estimated_identity <- exp((1.0 + log(2.0 * x$jaccard/(1.0+x$jaccard)))-1.0)
+x$estimated_identity <- exp((1.0 + log(2.0 * x$jaccard/(1.0+x$jaccard)))-1.0)
 
-#x <- x[x$estimated_identity >= estimated_identity_threshold,]
+x <- x[x$estimated_identity >= estimated_identity_threshold,]
 
 colors <- c("#F8766D", "#A3A500", "#00BF7D", "#00B0F6", "#E76BF4")
 if (length(unique(x$target)) > 5) {
