9aa87643a6b255d1b0c5083e21748f35077ee1bf
AndreaGuarracino <andreaguarracino@outlook.com>
2022-12-10 20:52:17 -0600

    script to plot PRDM9 hits on the whole chromosome

---
+++ scripts/plot_PRDM9_hits_without_annotation.all_chromosomes.R
@@ -0,0 +1,60 @@
+args <- commandArgs()
+path_fimo_window_bed <- args[6]
+width <- as.numeric(args[7])
+path_output <- args[8]
+
+library(ggridges)
+library(ggplot2)
+library(ggsci)
+library(tidyverse)
+library(scales) # for pretty_breaks()
+
+options(scipen = 9)
+
+x <- read.delim(path_fimo_window_bed, header = F)
+colnames(x) <- c('chrom', 'ref.begin', 'ref.end', 'hits')
+
+#x$chrom <- gsub('[:].*$','', x$chrom)
+
+colors <- c("#F8766D", "#A3A500", "#00BF7D", "#00B0F6", "#E76BF4")
+
+xx <- x
+p <- ggplot(xx, aes(
+  x = (ref.begin + (ref.end - ref.begin) / 2) / 1000000, width = ref.end - ref.begin,
+  y=hits,
+  color=chrom)
+) +
+  geom_step() +
+  facet_wrap(~chrom, scales = "free_y", ncol=1, labeller = labeller(variable = labels)) +
+  theme_bw() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    
+    text = element_text(size = 18),
+    axis.text.x = element_text(size = 12, angle = 45, vjust = 1.0, hjust=1),
+    axis.text.y = element_text(size = 12),
+    
+    legend.title = element_text(size = 18),
+    legend.text = element_text(size = 18),
+    legend.position = "right",
+    
+    strip.text.x = element_blank(),
+    strip.text.y = element_blank(),
+    plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm")
+  ) + scale_x_continuous(
+    #limits = c(x_min, x_max),
+    breaks = pretty_breaks(n=20),
+    expand = c(0.01, 0.01)) + labs(
+      x = paste('Position (Mbp)'),
+      y = paste('PRDM9 motifs\' hits'),
+      color = "Chromosome"
+    ) + scale_y_continuous(
+      #limits = c(0, max(10, max(xx$hits))),
+      breaks=pretty_breaks(n=6)
+    ) +
+  #ggtitle('PRDM9 density hits for each bp in the SST1 repetitive unit') +
+  scale_color_manual(values=colors) +
+  guides(colour = guide_legend(override.aes = list(size=10)))
+p
+ggsave(plot = p, path_output, width = width, height = length(unique(x$chrom))*4, units = "cm", dpi = 300, bg = "transparent", limitsize = FALSE)
+
